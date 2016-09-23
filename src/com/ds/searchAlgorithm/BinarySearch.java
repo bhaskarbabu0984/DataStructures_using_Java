02232a6c6619cd4c0b7ef9a64a6d99cfd6e1a183
bhaskar <bhaskarbabu0984@gmail.com>
2016-09-22 20:36:13 -0400

    Binary Search and Linear Search Algorithm
    Binary Search and Linear Search Algorithm

---
+++ src/com/ds/searchAlgorithm/BinarySearch.java
@@ -0,0 +1,50 @@
+package com.ds.searchAlgorithm;
+
+public class BinarySearch {
+	public static void main(String[] args) {
+		/*
+		 * By default we have the element sorted for simplicity. Else write any
+		 * sorting algorithm if needed for sorting
+		 */
+		int[] searchArray = { 0, 1, 2, 12, 34, 45, 100, 104, 126, 138 };
+
+		BinarySearch binarySearch = new BinarySearch();
+
+		// Start Binary search - SearchElement not in array - returns -1
+		int searchElement = 111;
+		int result = binarySearch.doBinarySearch(searchArray, searchElement);
+		System.out.println(" search element = " + searchElement + " is present at index - " + result);
+
+		// Search another element
+		searchElement = 126;
+		result = binarySearch.doBinarySearch(searchArray, searchElement);
+		System.out.println(" search element = " + searchElement + " is present at index - " + result);
+
+		// Search one more element
+		searchElement = 12;
+		result = binarySearch.doBinarySearch(searchArray, searchElement);
+		System.out.println(" search element = " + searchElement + " is present at index - " + result);
+	}
+
+	public int doBinarySearch(int[] array, int searchElement) {
+		int startIndex = 0;
+		int endIndex = array.length - 1;
+		
+		while(startIndex <= endIndex) {
+			int midIndex = (startIndex + endIndex) / 2;
+			if(searchElement == array[midIndex]){
+				//Search Element found in mid index position
+				return midIndex;
+			} else if(searchElement > array[midIndex]){
+				//SearchElement is after the midIndex
+				startIndex = midIndex + 1; 
+			} else {
+				//SearchElement is before the midIndex
+				endIndex = midIndex - 1;
+			}
+		}
+		//If no search element found in array, return -1
+		return -1;
+	}
+
+}
---
+++ src/com/ds/searchAlgorithm/LinearSearch.java
@@ -0,0 +1,33 @@
+package com.ds.searchAlgorithm;
+
+public class LinearSearch {
+
+	public static void main(String[] args) {
+		int [] arrayToSearch = {12, 34, 45, 2, 1, 0};
+		int searchElement = 45;
+		
+		//start searching
+		LinearSearch linearSearch = new LinearSearch();
+		
+		//Element found scenario - returns position of the element in array
+		int result = linearSearch.doLinearSearch(arrayToSearch, searchElement);
+		System.out.println("Search result = " + result);
+		
+		
+		//Element NOT found scenario - returns -1
+		searchElement = 29;
+		result = linearSearch.doLinearSearch(arrayToSearch, searchElement);
+		System.out.println("New Search result = " + result);
+		
+	}
+	
+	public int doLinearSearch(int [] array, int searchElement) {
+		for(int i = 0; i < array.length; i++){
+			if(searchElement == array[i]){
+				return i;
+			}
+		}
+		return -1;
+	}
+
+}
